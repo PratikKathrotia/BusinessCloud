ab8fb888f3e0a3c07a8db5bf0efd90552d1c54d8
Pratik1005 <intouchpratik94@gmail.com>
2019-03-21 21:13:06 -0700

    clean-up the app

--- src/libs/ui-global/public_api.ts
+++
@@ -1,6 +0,0 @@
-export * from './footer/footer.component';
-export * from './sidebar/sidebar.component';
-export * from './siderail/siderail.component';
-export * from './toolbar/toolbar.component';
-export * from './action-bar/action-bar.component';
-export * from './global.module';
