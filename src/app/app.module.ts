e1f7838f3c10869d5218913ebaabdab3cc8bbd78
Pratik1005 <intouchpratik94@gmail.com>
2019-04-16 21:29:18 -0700

    add method to generate db id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
+/**
+ * firebase modules
+ */
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+/**
+ * NgRx modules
+ */
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+/**
+ * forms and formly modules
+ */
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 
+/**
+ * library modules (src/libs/**)
+ */
 import { UiGlobalModule } from '@angular-cm/ui-global';
 import { UiMaterialModule } from '@angular-cm/ui-material';
 
+/**
+ * components imports and others
+ */
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 
@@ -26,6 +44,8 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
     ReactiveFormsModule,
     FormlyModule.forRoot(),
     FormlyMaterialModule,
--- src/libs/sys-utils/services/utility.service.ts
+++ src/libs/sys-utils/services/utility.service.ts
@@ -14,4 +14,14 @@ export class UtilityService {
     const copiedObj = JSON.parse(JSON.stringify(obj));
     return copiedObj;
   }
+
+  generateDatabaseId() {
+    const length = 20;
+    const chars = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    let result = '';
+    for (let i = length; i > 0; --i) {
+      result += chars[Math.round(Math.random() * (chars.length - 1))];
+    }
+    return result;
+  }
 }
